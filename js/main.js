8b6e126b9d7f22469f5d4155ea7102ec834a9859
thanatchakromsang <thanatchakromsang@gmail.com>
2018-01-03 20:56:42 +0700

    Initial branch

---
+++ js/main.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+   Particles.init({
+     selector: '.background',
+     connectParticles: true,
+     minDistance: 90,
+     color: '#123456',
+   });
+ };
+
+ var typed = new Typed('.typed', {
+  strings: ["a web developer.", "a mobile app developer","an infrastructure engineer.", "a linux freak.", "a vim enthusiast."],
+  typeSpeed: 50,
+  startDelay: 500,
+  backDelay: 500,
+  backSpeed: 40,
+  showCursor: false,
+  cursorChar: "▋",
+  loop: true,
+});
+
+ var terminal1 = new Typed('.terminal1', {
+  strings: ["whoami ^1000 <br> `Full Name : Thanatchaya Kromsaeng <br> Age : 21 years old <br> Education : KMITL Engineering class of 2015-2019 <br> Nationality : Thai <br> Language : Thai (Native), &nbsp;English (Intermediate) <br> Email : thanatchakromsang@gmail.com <br> Contact : +66882978450` <br> `<span class='prompt'>thanatchaya@local:~$ &nbsp</span>`^10000"],
+  typeSpeed: 70,
+  startDelay: 1000,
+  showCursor: false,
+  cursorChar: "▋",
+  onComplete: function(self) { terminal1.reset() },
+});
