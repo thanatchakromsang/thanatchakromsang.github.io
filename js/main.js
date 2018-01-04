435fd88e78d4b8626e3f88bd4ef2421a35d7a5fc
thanatchakromsang <thanatchakromsang@gmail.com>
2018-01-04 13:16:14 +0700

    change container to fluid

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-window.onload = function() {
-   Particles.init({
-     selector: '.background',
-     connectParticles: true,
-     minDistance: 90,
-     color: '#123456',
-   });
- };
+// window.onload = function() {
+//    Particles.init({
+//      selector: '.background',
+//      connectParticles: true,
+//      minDistance: 90,
+//      color: '#123456',
+//    });
+//  };
 
  var typed = new Typed('.typed', {
   strings: ["a web developer.", "a mobile app developer","an infrastructure engineer.", "a linux freak.", "a vim enthusiast."],
@@ -19,7 +19,7 @@ window.onload = function() {
 });
 
  var terminal1 = new Typed('.terminal1', {
-  strings: ["whoami ^1000 <br> `Full Name : Thanatchaya Kromsaeng <br> Age : 21 years old <br> Education : KMITL Engineering class of 2015-2019 <br> Nationality : Thai <br> Language : Thai (Native), &nbsp;English (Intermediate) <br> Email : thanatchakromsang@gmail.com <br> Contact : +66882978450` <br> `<span class='prompt'>thanatchaya@local:~$ &nbsp</span>`^10000"],
+  strings: ["whoami^1000 <br>`Full Name : Thanatchaya Kromsaeng <br> Age : 21 years old <br> Education : KMITL Engineering class of 2015-2019 <br> Nationality : Thai <br> Language : Thai (Native), &nbsp;English (Intermediate) <br> Email : thanatchakromsang@gmail.com <br> Contact : +66882978450` <br> `<span class='prompt'>thanatchaya@local:~$ </span>`^10000"],
   typeSpeed: 70,
   startDelay: 1000,
   showCursor: false,
